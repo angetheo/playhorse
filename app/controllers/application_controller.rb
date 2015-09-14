8ca9bfd1c08fdbe1de1278ef91739bcc5e37dbea
angetheo <angelotomasco@gmail.com>
2015-09-15 00:09:45 +0200

    refactoring, rails style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,15 +9,6 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def twitter_login
-  	Twitter::REST::Client.new do |config|
-  		config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
-  		config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
-  		config.access_token = ENV['TWITTER_ACCESS_TOKEN']
-  		config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
-		end
-	end
-
   def contact
     options = {
       :to => 'supporto@playhorsefarm.com',
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   	@articles = Article.all.sort_by(&:created_at).reverse.first(3)
   	@services = Service.all.sort_by(&:id)
 
-  	@last_tweet = twitter_login.user_timeline('playhorsefarm', {count: 1})[0]
+  	@last_tweet = $twitter.user_timeline('playhorsefarm', {count: 1})[0]
   	@last_instagram = HTTParty.get("https://api.instagram.com/v1/users/1902462206/media/recent/?client_id=#{ENV['INSTAGRAM_CLIENT_ID']}")['data'].first
   end
 end
