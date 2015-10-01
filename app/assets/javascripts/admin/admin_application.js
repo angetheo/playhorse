2de10bfde9b69c9f01aab7b5a2f7589a331e65a2
angetheo <angelotomasco@gmail.com>
2015-10-01 14:50:23 +0200

    tinymce theme not working

--- app/assets/javascripts/admin/admin_application.js
+++ app/assets/javascripts/admin/admin_application.js
@@ -18,6 +18,7 @@
 //= require parsley
 //= require easypiechart
 //= require tinymce/tinymce.min
+//= require tinymce/themes/modern/theme.min
 //= require repute
 //= require turbolinks
 //= require_tree .
