5faee8e80db06ddb39d9fd00585f6dedac9babd1
angetheo <angelotomasco@gmail.com>
2015-10-05 16:54:27 +0200

    rescues error if the last instagram post doesnt have a caption

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,16 @@ class HomeController < ApplicationController
   	@services = Service.all.sort_by(&:id)
 
   	@last_tweet = $twitter.user_timeline('playhorsefarm', {count: 1})[0]
-  	@last_instagram = HTTParty.get("https://api.instagram.com/v1/users/1902462206/media/recent/?client_id=#{ENV['INSTAGRAM_CLIENT_ID']}")['data'].first
+  	@last_instagram = initialize_instagram
+  end
+
+  private
+
+  def initialize_instagram
+  	@last_post = HTTParty.get("https://api.instagram.com/v1/users/1902462206/media/recent/?client_id=#{ENV['INSTAGRAM_CLIENT_ID']}")['data'].first
+  	@last_post['caption'] ||= { 'text' => "Seguici su instagram per gli ultimi aggiornamenti su Play Horse Farm! " }
+  	@last_post['caption']['text'] += '...' if @last_post['caption']['text'].size > 200
+
+  	return @last_post
   end
 end
